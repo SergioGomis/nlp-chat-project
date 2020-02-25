b826780a7ea2cfbc6edd0be6fa8472579563502f
Sergio Gomis <sgomis@sonneil.com>
2020-02-25 09:24:45 +0100

    fin v1.0

--- src/database.py
+++ src/database.py
@@ -22,13 +22,20 @@ nltk.download('vader_lexicon')
 
 class MongoObject():
     def __init__(self):
-        #self.myclient = client.get_default_database()["emotions"]
+        '''
+        Crea un objeto de la clase con los parámetros de conexión y las colecciones establecidas
+        '''
         self.mydb = client.get_default_database()
         self.userColl = self.mydb['users']
         self.converColl = self.mydb['conversations']
 
     @jsonErrorHandler
     def newUser(self,name):
+        '''
+        Crea un usuario
+        PARAMS:
+        name -> nombre (string)
+        '''
         mydict = {
                 "_id": self.maxUserId(),
                 "name": name }
@@ -40,12 +47,20 @@ class MongoObject():
 
     @jsonErrorHandler
     def getRandUsers(self,qty):
+        '''
+        Devuelve una lista de usuarios aleatorios de la BBDD.
+        PARAMS:
+        qty -> cantidad a obtener (int)
+        '''
         users_tot = self.userColl.find({})
         dev = random.sample(list(users_tot),int(qty))
         return dumps(dev)
 
     @jsonErrorHandler
     def maxUserId(self):
+        '''
+        Devuelve el siguiente id a utilizar en la colección de usuarios
+        '''
         try:
             n = list(self.userColl.find({},{"_id":1}).sort([('_id', -1)]).limit(1))[0]['_id']
             return n + 1
@@ -54,18 +69,34 @@ class MongoObject():
     
     @jsonErrorHandler
     def getUserByName(self,name):
+        '''
+        Devuelve el id de un usuario.
+        PARAMS:
+        name -> nombre (string)
+        '''
         print('LOG: Buscando a',name)
         dev = self.userColl.find_one({"name":name})
         return dev
 
     @jsonErrorHandler
     def getNameById(self,userId):
+        '''
+        Devuelve el nombre de un usuario a partir de su id
+        PARAMS:
+        userId -> id usuario (int)
+        '''
         print('LOG: Buscando id',userId)
         dev = self.userColl.find_one({"_id":userId})
         return dev
 
     @jsonErrorHandler
     def newChat(self,nombre,usuarios):
+        '''
+        Crea un nuevo chat.
+        PARAMS:
+        nombre -> nombre del chat (string)
+        usuarios -> lista con los nombres de usuarios a añadir ([string, string...])
+        '''
         idusuarios = [self.getUserByName(usu) for usu in usuarios]
         mydict = {
                 "_id": self.maxChatId(),
@@ -78,6 +109,11 @@ class MongoObject():
 
     @jsonErrorHandler
     def chatExists(self,chat_id):
+        '''
+        Verifica si existe un chat por su id
+        PARAMS:
+        chat_id -> id del chat (int)
+        '''
         print('LOG: Buscando chat',chat_id)
         dev = self.converColl.find_one({"_id":int(chat_id)})
         if not dev:
@@ -87,6 +123,12 @@ class MongoObject():
 
     @jsonErrorHandler
     def addUser2chat(self,chat_id,usuario):
+        '''
+        Añade un usuario a un chat.
+        PARAMS:
+        chat_id -> id del chat (int)
+        usuario -> nombre del usurio (string)
+        '''
         print('LOG:',chat_id,usuario)
         if self.chatExists(chat_id) != 0:
             idUsu = self.getUserByName(usuario)
@@ -103,6 +145,12 @@ class MongoObject():
 
     @jsonErrorHandler
     def userInChat(self,chat_id,idusu):
+        '''
+        Verifica si un usuario está incluido en el chat
+        PARAMS:
+        chat_id -> id del chat (int)
+        idusu -> id del usuario (int)
+        '''
         print('LOG: Buscando usuario en el chat',chat_id)
         dev = self.converColl.find_one({"_id":int(chat_id),"usuarios":int(idusu)})
         if not dev:
@@ -112,6 +160,13 @@ class MongoObject():
 
     @jsonErrorHandler
     def addMsg2chat(self,chat_id,usuario,mensaje):
+        '''
+        Añade un mensaje a un chat
+        PARAMS:
+        chat_id -> id del chat (int)
+        usuario -> nombre del usuario (string)
+        mensaje -> texto del mensaje (string)
+        '''
         print('LOG:',chat_id,usuario,mensaje)
         if self.chatExists(chat_id) != 0:
             idUsu = self.getUserByName(usuario)
@@ -131,6 +186,9 @@ class MongoObject():
 
     @jsonErrorHandler
     def maxChatId(self):
+        '''
+        Devuelve el siguiente id a utilizar en la colección de chats
+        '''
         try:
             n = list(self.converColl.find({},{"_id":1}).sort([('_id', -1)]).limit(1))[0]['_id']
             return n + 1
@@ -139,16 +197,32 @@ class MongoObject():
 
     @jsonErrorHandler
     def showChatMsg(self,chat_id):
+        '''
+        Devuelve todos los mensajes de un chat.
+        PARAMS:
+        chat_id -> id del chat (int) 
+        '''
         query = self.converColl.find({"_id":int(chat_id)},{"_id":0,"mensajes":1})
         return query[0]
 
     @jsonErrorHandler
     def changeChatName(self,chat_id,nuevonombre):
+        '''
+        Cambia el nombre de un chat.
+        PARAMS:
+        chat_id -> id del chat (int)
+        nuevonombre -> nuevo nombre para el chat (string) 
+        '''
         x = self.converColl.update({ "_id": int(chat_id) },{ "$set": { "nombre": nuevonombre } })
         return x
 
     @jsonErrorHandler
     def getSentiment(self,chat_id):
+        '''
+        Devuelve los parámetros de análisis de sentimiento de un chat.
+        PARAMS:
+        chat_id -> id del chat (int) 
+        '''
         x = self.converColl.find({"_id":int(chat_id)},{"_id":0,"mensajes":1})
         x = ' '.join(t['texto'] for t in x[0]['mensajes'])
         sia = SentimentIntensityAnalyzer()
@@ -158,6 +232,11 @@ class MongoObject():
 
     @jsonErrorHandler
     def getRecommendations(self,usu_recom):
+        '''
+        Obtiene el top-3 de usuarios afines a un usuario determinado.
+        PARAMS:
+        usu_recom -> nombre del usuario a evaluar (string) 
+        '''
         x = self.converColl.find({},{"_id":0,"mensajes":1})
         usuarios = []
         mensajes = []
